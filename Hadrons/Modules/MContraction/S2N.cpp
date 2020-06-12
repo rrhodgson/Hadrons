fc41f78e19acc69ad92956b163f6399a8bbd6c0c
Raoul Hodgson <raoul.hodgson@ed.ac.uk>
2020-06-12 12:04:03 +0100

    Added S2N and S2N_2 codes

---
+++ Hadrons/Modules/MContraction/S2N.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MContraction/S2N.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MContraction;
+
+template class Grid::Hadrons::MContraction::TS2N<FIMPL>;
---
+++ Hadrons/Modules/MContraction/S2N_2.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MContraction/S2N_2.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MContraction;
+
+template class Grid::Hadrons::MContraction::TS2N_2<FIMPL>;
