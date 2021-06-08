8ddc4ef51146a02086f458f2bdfb8ccab13789ef
Raoul Hodgson <raoul.hodgson@ed.ac.uk>
2021-06-08 12:04:59 +0100

    Epack load bugfix

--- Hadrons/Modules/MIO/LoadEigenPack.hpp
+++ Hadrons/Modules/MIO/LoadEigenPack.hpp
@@ -80,7 +80,7 @@ MODULE_REGISTER_TMP(LoadFermionEigenPack, ARG(TLoadEigenPack<FermionEigenPack<FI
 #ifdef GRID_DEFAULT_PRECISION_DOUBLE
 MODULE_REGISTER_TMP(LoadFermionEigenPackF, ARG(TLoadEigenPack<FermionEigenPack<FIMPLF>, GIMPLF>), MIO);
 MODULE_REGISTER_TMP(LoadFermionEigenPackIo32, ARG(TLoadEigenPack<FermionEigenPack<FIMPL, FIMPLF>, GIMPL>), MIO);
-MODULE_REGISTER_TMP(LoadFermionEigenPackIo64, ARG(TLoadEigenPack<FermionEigenPack<FIMPLF, FIMPL>, GIMPL>), MIO);
+MODULE_REGISTER_TMP(LoadFermionEigenPackIo64, ARG(TLoadEigenPack<FermionEigenPack<FIMPLF, FIMPL>, GIMPLF>), MIO);
 #endif
 
 /******************************************************************************
