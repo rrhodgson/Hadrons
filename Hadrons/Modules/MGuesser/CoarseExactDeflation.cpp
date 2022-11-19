d5d1da9bfa8832ea69ca7ff05c42cf07dc89dc5a
Raoul Hodgson <raoul.hodgson@ed.ac.uk>
2022-11-19 20:20:22 +0000

    Add LC deflation

---
+++ Hadrons/Modules/MGuesser/CoarseExactDeflation.cpp
@@ -0,0 +1,15 @@
+#include <Hadrons/Modules/MGuesser/CoarseExactDeflation.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MGuesser;
+
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL,HADRONS_DEFAULT_LANCZOS_NBASIS>>;
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL,150>>;
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL,250>>;
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL,400>>;
+
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,HADRONS_DEFAULT_LANCZOS_NBASIS>>;
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,150>>;
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,250>>;
+template class Grid::Hadrons::MGuesser::TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,400>>;
---
+++ Hadrons/Modules/MGuesser/CoarseExactDeflation.hpp
@@ -0,0 +1,115 @@
+#ifndef Hadrons_MGuesser_CoarseExactDeflation_hpp_
+#define Hadrons_MGuesser_CoarseExactDeflation_hpp_
+
+#include <Hadrons/Global.hpp>
+#include <Hadrons/Module.hpp>
+#include <Hadrons/ModuleFactory.hpp>
+#include <Hadrons/EigenPack.hpp>
+
+BEGIN_HADRONS_NAMESPACE
+
+/******************************************************************************
+ *                        Exact deflation guesser                             *
+ ******************************************************************************/
+BEGIN_MODULE_NAMESPACE(MGuesser)
+
+class CoarseExactDeflationPar: Serializable
+{
+public:
+    GRID_SERIALIZABLE_CLASS_MEMBERS(CoarseExactDeflationPar,
+                                    std::string, eigenPack,
+                                    unsigned int, size);
+};
+
+template <typename EPack>
+class TCoarseExactDeflation: public Module<CoarseExactDeflationPar>
+{
+public:
+    typedef typename EPack::Field Field;
+    typedef typename EPack::CoarseField CoarseField;
+public:
+    // constructor
+    TCoarseExactDeflation(const std::string name);
+    // destructor
+    virtual ~TCoarseExactDeflation(void) {};
+    // dependency relation
+    virtual std::vector<std::string> getInput(void);
+    virtual std::vector<std::string> getOutput(void);
+    virtual DependencyMap getObjectDependencies(void);
+    // setup
+    virtual void setup(void);
+    // execution
+    virtual void execute(void);
+};
+
+MODULE_REGISTER_TMP(CoarseExactDeflation    , ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL ,HADRONS_DEFAULT_LANCZOS_NBASIS>>), MGuesser);
+MODULE_REGISTER_TMP(CoarseExactDeflation150 , ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL ,150>>), MGuesser);
+MODULE_REGISTER_TMP(CoarseExactDeflation250 , ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL ,250>>), MGuesser);
+MODULE_REGISTER_TMP(CoarseExactDeflation400 , ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPL ,400>>), MGuesser);
+
+
+MODULE_REGISTER_TMP(CoarseExactDeflationF   , ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,HADRONS_DEFAULT_LANCZOS_NBASIS>>), MGuesser);
+MODULE_REGISTER_TMP(CoarseExactDeflationF150, ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,150>>), MGuesser);
+MODULE_REGISTER_TMP(CoarseExactDeflationF250, ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,250>>), MGuesser);
+MODULE_REGISTER_TMP(CoarseExactDeflationF400, ARG(TCoarseExactDeflation<CoarseFermionEigenPack<FIMPLF,400>>), MGuesser);
+
+
+/******************************************************************************
+ *                 TExactDeflation implementation                             *
+ ******************************************************************************/
+// constructor /////////////////////////////////////////////////////////////////
+template <typename EPack>
+TCoarseExactDeflation<EPack>::TCoarseExactDeflation(const std::string name)
+: Module<CoarseExactDeflationPar>(name)
+{}
+
+// dependencies/products ///////////////////////////////////////////////////////
+template <typename EPack>
+std::vector<std::string> TCoarseExactDeflation<EPack>::getInput(void)
+{
+    std::vector<std::string> in = {par().eigenPack};
+    
+    return in;
+}
+
+template <typename EPack>
+std::vector<std::string> TCoarseExactDeflation<EPack>::getOutput(void)
+{
+    std::vector<std::string> out = {getName()};
+    
+    return out;
+}
+
+template <typename EPack>
+DependencyMap TCoarseExactDeflation<EPack>::getObjectDependencies(void)
+{
+    DependencyMap dep;
+    
+    dep.insert({par().eigenPack, getName()});
+
+    return dep;
+}
+
+// setup ///////////////////////////////////////////////////////////////////////
+template <typename EPack>
+void TCoarseExactDeflation<EPack>::setup(void)
+{
+    LOG(Message) << "Setting exact deflation guesser with eigenpack '"
+                 << par().eigenPack << std::endl;
+    
+    auto &epack = envGet(EPack, par().eigenPack);
+    envCreateDerived(LinearFunction<Field>, ARG(LocalCoherenceDeflatedGuesser<Field, CoarseField>), getName(),
+                     env().getObjectLs(par().eigenPack), epack.evec, epack.evecCoarse, epack.evalCoarse);
+
+}
+
+// execution ///////////////////////////////////////////////////////////////////
+template <typename EPack>
+void TCoarseExactDeflation<EPack>::execute(void)
+{}
+
+END_MODULE_NAMESPACE
+
+END_HADRONS_NAMESPACE
+
+#endif // Hadrons_MGuesser_CoarseExactDeflation_hpp_
