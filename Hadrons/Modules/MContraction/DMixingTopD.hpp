c30d8fb94db7c7698ca22931fe6cd6f97ec89508
felixerben <felix.erben@cern.ch>
2025-09-26 13:41:50 +0100

    builds, parity outermost loop

--- Hadrons/Modules/MContraction/DMixingTopD.hpp
+++ Hadrons/Modules/MContraction/DMixingTopD.hpp
@@ -264,60 +264,41 @@ void TDMixingTopD<FImpl>::execute(void)
 
     int Neta = ql1.size();
 
-    //std::map<std::string, std::vector<SpinColourMatrixD>> half_if;
-    //std::map<std::string, std::vector<SpinColourMatrixD>> half_fi;
-    std::vector<std::vector<std::vector<SpinColourMatrixD>>> half_lr( Neta, std::vector<std::vector<SpinColourMatrixD>>(2, std::vector<SpinColourMatrixD>(2)));
-    std::vector<std::vector<std::vector<SpinColourMatrixD>>> half_rl( Neta, std::vector<std::vector<SpinColourMatrixD>>(2, std::vector<SpinColourMatrixD>(2)));
+    std::vector<std::vector<std::vector<SpinColourMatrixD>>> half_lr(Neta, std::vector<std::vector<SpinColourMatrixD>>(2, std::vector<SpinColourMatrixD>(Nt)));
+    std::vector<std::vector<std::vector<SpinColourMatrixD>>> half_rl(Neta, std::vector<std::vector<SpinColourMatrixD>>(2, std::vector<SpinColourMatrixD>(Nt)));
 
 
-    //map<std::string, Gamma> parityG;
-    //parityG.emplace("+",Gamma(Gamma::Algebra::Identity));
-    //parityG.emplace("-",Gamma(Gamma::Algebra::Gamma5));
 
-    /*
-    std::vector<LatticeSpinColourMatrixD> GdsG_pp(2*Neta, grid);
-    for (int i=0; i<Neta; i++) 
-    {
-	auto tmp = GH_VVAA_cap(*ql1[i]);
-	GdsG_pp[i] = tmp.first; // r1 
-	GdsG_pp[i + Neta] = tmp.second;  // r2 
-    }
-    */
-
-    std::vector<Gamma> parityG(2);
-    parityG[0] = Gamma(Gamma::Algebra::Identity); // parity +
-    parityG[1] = Gamma(Gamma::Algebra::Gamma5);   // parity -
+    // parity +, parity -
+    std::vector<Gamma> parityG = {Gamma(Gamma::Algebra::Identity),Gamma(Gamma::Algebra::Gamma5)};
     std::vector<LatticeSpinColourMatrixD> GdsG_pp(2, grid);
-    for (int i=0; i<Neta; i++) 
+    for (int p = 0; p < 2; p++) 
     {
-	// here one has to add ql2 if one wants them to be allowed to be different
-	auto tmp = GH_VVAA_cap(*ql1[i]);  	
-	GdsG_pp[0] = tmp.first;  // r1 
-	GdsG_pp[1] = tmp.second; // r2 
-        for (int r = 0; r < 2; r++) 
-	{
-	    for (int p = 0; p < 2; p++) 
+        for (int i=0; i<Neta; i++) 
+        {
+            // here one has to add ql2 if one wants them to be allowed to be different
+            auto tmp = GH_VVAA_cap(*ql1[i]);  	
+            GdsG_pp[0] = tmp.first;  // r1 
+            GdsG_pp[1] = tmp.second; // r2 
+            for (int r = 0; r < 2; r++) 
 	    {
-	        half_lr[i][r][p] = contract_D_half(qcl, qur, GdsG_pp[i + Neta * (r-1)] * parityG.at(p));
-	        half_rl[i][r][p] = contract_D_half(qcr, qul, GdsG_pp[i + Neta * (r-1)] * parityG.at(p));
+	        half_lr[i][r] = contract_D_half(qcl, qur, GdsG_pp[i + Neta * (r-1)] * parityG.at(p));
+	        half_rl[i][r] = contract_D_half(qcr, qul, GdsG_pp[i + Neta * (r-1)] * parityG.at(p));
 	    }
 	}
-    }
     
-    for (int r = 0; r < 2; r++) 
-    {
-        for (int s = 0; r < 2; r++) 
-	{
-            res.info.rr = std::to_string(r+1) + std::to_string(s+1);
-	    for (int p = 0; p < 2; p++) 
+        for (int r = 0; r < 2; r++) 
+        {
+            for (int s = 0; r < 2; r++) 
 	    {
+                res.info.rr = std::to_string(r+1) + std::to_string(s+1);
                 res.info.parity = (p == 0) ? "+" : "-";
 		std::vector<std::vector<std::vector<std::vector<ComplexD>>>> buf(Neta, std::vector<std::vector<std::vector<ComplexD>>>(Neta, std::vector<std::vector<ComplexD>>(Nt, std::vector<ComplexD>(Nt))));
 		for (int i=0; i<Neta; i++) 
 		{
 		    for (int j=0; j<Neta; j++) 
 		    {
-			buf[i][j] = contract_D(half_lr[i][r][p], half_rl[s][j][p]);
+			buf[i][j] = contract_D(half_lr[i][r], half_rl[j][s]);
 		    }
 		}
 
@@ -363,88 +344,6 @@ void TDMixingTopD<FImpl>::execute(void)
     auto &out = envGet(HadronsSerializable, getName());
     out = result;
 
-
-
-   /*
-
-    Gamma               g5(Gamma::Algebra::Gamma5);
-    Gamma               gVX(Gamma::Algebra::GammaX);
-    Gamma               gVY(Gamma::Algebra::GammaY);
-    Gamma               gVZ(Gamma::Algebra::GammaZ);
-    Gamma               gVT(Gamma::Algebra::GammaT);
-    Gamma               gAX(Gamma::Algebra::GammaXGamma5);
-    Gamma               gAY(Gamma::Algebra::GammaYGamma5);
-    Gamma               gAZ(Gamma::Algebra::GammaZGamma5);
-    Gamma               gAT(Gamma::Algebra::GammaTGamma5);
-    std::vector<Gamma>      GV = {gVX, gVY, gVZ, gVT};
-    std::vector<Gamma>      GA = {gAX, gAY, gAZ, gAT};
-
-    envGetTmp(ComplexField, corr);
-    envGetTmp(PropagatorField, parPlusR1L1);
-    envGetTmp(PropagatorField, parPlusR2L1);
-    envGetTmp(PropagatorField, parMinusR1L1);
-    envGetTmp(PropagatorField, parMinusR2L1);
-    envGetTmp(PropagatorField, parPlusR1L2);
-    envGetTmp(PropagatorField, parPlusR2L2);
-    envGetTmp(PropagatorField, parMinusR1L2);
-    envGetTmp(PropagatorField, parMinusR2L2);
-
-    SlicedComplex buf;
-
-    // rr'=11  (one single big trace)
-    // for the moment, all gamma5 written explicitly - can simplyfy this
-    // corr = tr(g5*g5*adj(qcl)*g5*G11*ql1*G12*qur*g5*g5*adj(qcr)*g5*G21*ql2*G22*qul); 
-   
-    // VV
-    for (const auto &G: GV)
-    {
-        auto obj1 = G*ql1*G;
-	parPlusR1L1 += obj1;
-	obj1 = G*ql2*G;
-	parPlusR1L2 += obj1;
-        //auto obj2 = G*trace(ql1*G);
-	//parPlusR2 += obj2;
-    }
-    // AA
-    for (const auto &G: GA)
-    {
-        auto obj1 = G*ql1*G;
-	parPlusR1L1 += obj1;
-	obj1 = G*ql2*G;
-	parPlusR1L2 += obj1;
-    }
-    parMinusR1L1 = parPlusR1L1 * g5;
-    parMinusR1L2 = parPlusR1L2 * g5;
-
-    // parity = + , rr = 11
-    corr = trace(g5*g5*adj(qcl)*parPlusR1L1*qur*g5*g5*adj(qcr)*g5*parPlusR1L2*qul);
-    sliceSum(corr, buf, Tp);
-    r.corr.clear();
-    for (unsigned int t = 0; t < buf.size(); ++t)
-    {
-        r.corr.push_back(TensorRemove(buf[t]));
-    }
-    r.info.parity = "+";
-    r.info.rr     = "11";
-    result.push_back(r);
-
-    // parity = - , rr = 11
-    corr = trace(g5*g5*adj(qcl)*parMinusR1L1*qur*g5*g5*adj(qcr)*g5*parMinusR1L2*qul);
-    sliceSum(corr, buf, Tp);
-    r.corr.clear();
-    for (unsigned int t = 0; t < buf.size(); ++t)
-    {
-        r.corr.push_back(TensorRemove(buf[t]));
-    }
-    r.info.parity = "-";
-    r.info.rr     = "11";
-    result.push_back(r);
-
-    // save result, and hand it to environment
-    saveResult(par().output, "DMixingTopD", result);
-    auto &out = envGet(HadronsSerializable, getName());
-    out = result;
-    */
 }
 
 
